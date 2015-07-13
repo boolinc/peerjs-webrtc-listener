c48170fe6f979194e666d6a63969565eef093dda
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-07-13 05:36:06 -0500

    Sent with defaultId

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
-(function () {
-    var peer = new Peer({key: 'lwjd5qra8257b9'});
+$(function () {
+    'use strict';
+
+    var peer = new Peer({ key: 'evycxpu0zuissjor' });
+
     peer.on('open', function (id) {
-        peer.connect('whoopsie');
+        $('h1').html('Hi ' + id + ' :)');
+        peer.connect('defaultId');
     });
 
     peer.on('call', function (call) {
+        call.on('stream', function (stream) {
+            var streamUrl = URL.createObjectURL(stream);
+            var audio = new Audio();
+            audio.src = streamUrl;
+            audio.play();
+        });
         call.answer();
     });
 
-})();
+    peer.on('error', function (err) {
+        console.error(err);
+    });
+
+});
